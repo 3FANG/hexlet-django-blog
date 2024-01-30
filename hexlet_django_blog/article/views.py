0cbece1c77ae1d806012f3f073f15feb81f629d3
3FANG <d.hasimikov@mail.ru>
2024-01-30 14:02:04 +0300

    Change some FBV(hexlet_django_blog.views.index and hexlet_django_blog.article.views.index) to CBV

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    return render(request, 'articles/index.html', context={'application': 'hexlet_django_blog.article'})
+class ArticleView(View):
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        return render(request, 'articles/index.html', context={'application': 'hexlet_django_blog.article'})
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,9 +1,17 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def index(request):
-    return render(request, 'index.html', context={
-        'who': 'World',
-    })
+
+class HomePageView(TemplateView):
+
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['who'] = "World"
+        return
+
 
 def about(request):
     return render(request, 'about.html')
