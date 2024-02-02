35e94d96397740f79dd9b15b3513d643bfadb0ec
3FANG <d.hasimikov@mail.ru>
2024-02-02 14:44:01 +0300

    Add admin and some functions in admin's panel

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp')
+    list_filter = (('timestamp', DateFieldListFilter),)
+    search_fields = ['name', 'body']
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,10 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from hexlet_django_blog.article.models import Article
+
 
 class ArticleView(View):
     def get(self, request, *args, **kwargs) -> HttpResponse:
-        return render(request, 'articles/index.html', context={'application': 'hexlet_django_blog.article'})
+        return render(request, 'articles/index.html', context={'articles': Article.objects.all()})
 
 
 def article(request, tags: str, article_id: int) -> HttpResponse:
