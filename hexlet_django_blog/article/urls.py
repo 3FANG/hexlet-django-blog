8a6c716d2222e6e4747ad79611482a3ec08dad1a
3FANG <d.hasimikov@mail.ru>
2024-02-08 13:43:13 +0300

    Add create article from form functional

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -4,6 +4,7 @@ from hexlet_django_blog.article import views
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('<int:id>/', views.ArticleView.as_view(), name='article_id')
+    path('', views.IndexView.as_view(), name='articles_index'),
+    path('<int:id>/', views.ArticleView.as_view(), name='articles_detail'),
+    path('create/', views.ArticleFormCreateView.as_view(), name='articles_create')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,13 +1,17 @@
+from typing import Any
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
 from hexlet_django_blog.article.models import Article
+from hexlet_django_blog.article.forms import ArticleForm
 
 
 class IndexView(View):
+    """Представление для домашней страницы статей (списка статей)."""
 
     def get(self, request, *args, **kwargs) -> HttpResponse:
+        print('yes' if request.GET else 'no')
         articles = Article.objects.all()[:15]
         return render(request, 'articles/index.html', context={
             'articles': articles
@@ -15,9 +19,29 @@ class IndexView(View):
 
 
 class ArticleView(View):
+    """Представление для конкретной статьи."""
 
     def get(self, request, *args, **kwargs) -> HttpResponse:
         article = get_object_or_404(Article, id=kwargs['id'])
         return render(request, 'articles/show.html', context={
             'article': article,
+        })
+
+
+class ArticleFormCreateView(View):
+    """Представление формы для создания новой статьи."""
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', context={
+            'form': form
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles_index')
+        return render(request, 'articles/create.html', context={
+            'form': form
         })
